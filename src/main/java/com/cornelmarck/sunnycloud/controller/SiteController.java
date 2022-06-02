31b9eb4f03248e15177c098a4f3c9abd17e1c857
Cornel Marck <cornel@cornelmarck.com>
2022-06-02 15:37:54 +0100

    Nest measurement endpoint in site

--- src/main/java/com/cornelmarck/sunnycloud/controller/SiteController.java
+++ src/main/java/com/cornelmarck/sunnycloud/controller/SiteController.java
@@ -1,21 +1,25 @@
 package com.cornelmarck.sunnycloud.controller;
 
+import com.cornelmarck.sunnycloud.config.DynamoDBDateTimeConverter;
+import com.cornelmarck.sunnycloud.model.Measurement;
 import com.cornelmarck.sunnycloud.model.Site;
+import com.cornelmarck.sunnycloud.repository.MeasurementRepository;
 import com.cornelmarck.sunnycloud.repository.SiteRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @RestController
 public class SiteController {
     private final SiteRepository siteRepository;
-
-    public SiteController(SiteRepository siteRepository) {
-        this.siteRepository = siteRepository;
-    }
+    private final MeasurementRepository measurementRepository;
+    private final DynamoDBDateTimeConverter dynamoDBDateTimeConverter;
 
     //Get all sites filtering on owner
     @GetMapping("/sites")
@@ -37,5 +41,21 @@ public class SiteController {
     public Site create(@RequestBody Site newSite) {
         siteRepository.insert(newSite);
         return newSite;
+    }
+
+    @GetMapping("/sites/{siteId}/measurements")
+    List<Measurement> allBySiteId(@PathVariable String siteId, @RequestParam Optional<String> from, @RequestParam Optional<String> to) {
+        if (siteRepository.findById(siteId).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Site not found: " + siteId);
+        }
+
+        try {
+            return measurementRepository.findAllBySiteIdAndTimestampBetween(siteId, from, to);
+        }
+        catch (DateTimeParseException dateTimeParseException) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid request");
+        }
+
+
     }
 }
--- src/main/java/com/cornelmarck/sunnycloud/model/Measurement.java
+++ src/main/java/com/cornelmarck/sunnycloud/model/Measurement.java
@@ -3,32 +3,37 @@ package com.cornelmarck.sunnycloud.model;
 import com.amazonaws.services.dynamodbv2.datamodeling.*;
 import com.cornelmarck.sunnycloud.config.DynamoDBDateTimeConverter;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
 @DynamoDBTable(tableName="Main")
 public class Measurement {
-    @Getter @Setter @DynamoDBHashKey(attributeName = "Id")
-    private String siteId;
     @Getter @Setter
-    @DynamoDBTypeConverted(converter = DynamoDBDateTimeConverter.class) @DynamoDBRangeKey(attributeName = "SortKey")
+    @DynamoDBHashKey(attributeName = "Id")
+    @JsonIgnore
+    private String siteId;
+
+    @Getter @Setter
+    @DynamoDBRangeKey(attributeName = "SortKey")
+    @DynamoDBTypeConverted(converter = DynamoDBDateTimeConverter.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private LocalDateTime timestamp;
+
     @Getter @Setter @DynamoDBAttribute(attributeName = "PowerOutput")
     private double powerOutput;
 
     public Measurement() {}
-    public Measurement(String siteId, LocalDateTime timestamp, double powerOutput) {
-        setSiteId(siteId);
-        setTimestamp(timestamp);
-        setPowerOutput(powerOutput);
-    }
+
 }
