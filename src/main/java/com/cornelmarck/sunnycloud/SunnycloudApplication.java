2ee2d4f379c71e96f183db73257646b10a686950
Cornel Marck <cornel@cornelmarck.com>
2022-07-01 21:37:15 +0100

    Enable scheduling

--- src/main/java/com/cornelmarck/sunnycloud/SunnycloudApplication.java
+++ src/main/java/com/cornelmarck/sunnycloud/SunnycloudApplication.java
@@ -1,16 +1,16 @@
 package com.cornelmarck.sunnycloud;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableScheduling
 @SpringBootApplication
 public class SunnycloudApplication {
     public static void main(String[] args) {
